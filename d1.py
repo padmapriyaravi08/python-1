cbd80ea20d8e04d55100cb958eddbd3cdf5cf820
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2025-09-10 17:41:16 +0530

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,8 @@
+d={}
+n=int(input("enter the number of key value pairs"))
+for i in range(n):
+    key=input("enter a key:")
+    value=input("enter a value:")
+    d[key]=value
+print("the dictionary is:",d)
+    
