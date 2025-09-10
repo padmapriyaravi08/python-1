242cd3a242b09a290cc417f238988d2c7af2d078
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2025-09-10 18:19:53 +0530

    Add files via upload

---
+++ func 2.py
@@ -0,0 +1,18 @@
+def greet(name):
+    print("welcome to coding:",name)
+greet("priya")
+
+
+
+
+def greet():
+    name=input("enter ur name")
+    print("welcome to coding:",name)
+greet()
+
+
+
+def greet():
+    name=input("enter your name:")
+    return name
+print("welcome to coding:",greet())
