f3044408516bd3bd63d763df0a1559086a9fb202
padmapriyaravi08 <padmapriyaravi08@gmail.com>
2025-09-16 14:15:01 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,16 @@
+s="python"
+new_s="j"+s[1:]
+print(new_s)
+
+
+
+
+s="hello world"
+updated=s[:6]+"python"
+print(updated)
+
+
+
+name="priya"
+age=18
+print(f"my name is {name} and i am {age}years old")
